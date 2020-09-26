51c12616d12184da705c5e0222329675b77dd7bc
Ruth Vang <r.vang012@gmail.com>
2020-09-25 20:16:01 -0500

    updating

--- Note-Taker/Develop/public/server.js
+++ Note-Taker/Develop/public/server.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded)({ extended: true});
 app.use(express.json());
 app.use(express.static("public"));
 
+let notes = [];
+
 app.get( "/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -17,6 +19,16 @@ app.get( "/", function (req, res) {
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/notes.html"));
 });
+
+app.get('/api/notes', function(req, res) {
+    res.json(notes);
+});
+
+app.post('/api/notes', function (req, res) {
+    let newNotes = req.body;
+    notes.push(newNotes);
+    res.json(notes);
+})
 
 app.listen(PORT, function () {
     console.log("App listening on PORT" + PORT);
