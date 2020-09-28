2e29f1bc5958da70a2a3ab937b00644a26153ffa
Ruth Vang <r.vang012@gmail.com>
2020-09-27 19:36:18 -0500

    adding routes

---
+++ Note-Taker/Develop/routes/apiroutes.js
@@ -0,0 +1,35 @@
+var fs = require("fs");
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        res.json(data);
+    })
+};
+
+app.get("/api.notes", function (req, res) {
+    res.json(date[Number(req.params.id)]);
+});
+
+app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    let uniqueId = data.length.toString();
+    
+    newNote.Id = uniqueId;
+    data.push(newNote);
+
+    res.json(data);
+});
+
+app.delete("/api/notes/:id", function (req, res){
+    let noteId = req.params.id;
+    let newId = 0;
+    data = data.filter((currentNote) => {
+        return currentNote.id != noteId;
+    });
+    for (currentNote of data) {
+        currenteNote.id = newId.toString();
+        newId++;
+    }
+    res.json(data);
+});
---
+++ Note-Taker/Develop/routes/htmlroutes.js
@@ -0,0 +1,11 @@
+var path = require("path");
+module.exports = function(app){
+    app.get( "/", function (req, res) {
+        res.sendFile(path.join(__dirname, "index.html"));
+    });
+    
+    app.get("/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "/notes.html"));
+    });
+    
+}
