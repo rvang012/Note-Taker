af922b7a9996a35b0f5f581c7405e8512e471db3
Ruth Vang <r.vang012@gmail.com>
2020-09-27 19:37:48 -0500

    updating

--- Note-Taker/Develop/public/server.js
+++ Note-Taker/Develop/public/server.js
@@ -1,34 +1,24 @@
-const { json } = require("express");
-
 var express = require("express");
 var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded)({ extended: true});
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-let notes = [];
-
-app.get( "/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/notes.html"));
-});
+require("./routes/htmlroutes")(app);
 
 app.get('/api/notes', function(req, res) {
     res.json(notes);
 });
 
 app.post('/api/notes', function (req, res) {
-    let newNotes = req.body;
-    notes.push(newNotes);
+   let newNotes = req.body;
+   notes.push(newNotes);
     res.json(notes);
-})
+ });
 
 app.listen(PORT, function () {
     console.log("App listening on PORT" + PORT);
