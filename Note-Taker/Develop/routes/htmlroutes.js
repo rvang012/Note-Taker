8cc373fc888b9deab255d60df7340774b6eb8db5
Ruth Vang <r.vang012@gmail.com>
2020-09-27 20:15:59 -0500

    updatinghtml

--- Note-Taker/Develop/routes/htmlroutes.js
+++ Note-Taker/Develop/routes/htmlroutes.js
@@ -1,11 +1,9 @@
 var path = require("path");
-module.exports = function(app){
-    app.get( "/", function (req, res) {
-        res.sendFile(path.join(__dirname, "index.html"));
-    });
-    
-    app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "/notes.html"));
-    });
-    
-}
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+};
